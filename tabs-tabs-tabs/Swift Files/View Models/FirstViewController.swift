01edea675fd9858c63a596d839ea7c0d059774bc
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2018-08-25 18:40:59 +0530

    Changes in TableView

---
+++ tabs-tabs-tabs/Swift Files/View Models/FirstViewController.swift
@@ -0,0 +1,47 @@
+//
+//  FirstViewController.swift
+//  tabs-tabs-tabs
+//
+//  Created by Aditya Vikram Godawat on 25/08/18.
+//  Copyright © 2018 Aditya Vikram Godawat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    //MARK: - Outlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+
+//MARK: - Table View Delegate and Data Source
+
+extension FirstViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! UITableViewCell
+        
+        cell.textLabel?.text = String((indexPath.row + 1) * 2) // Displays even numbers
+        
+        return cell
+    }
+}
